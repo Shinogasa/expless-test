565cc63cb8ef1e14a2103dc0ba28c16f869d0360
Shinonome <cuulbulu@gmail.com>
2018-03-19 02:01:56 +0000

    add data into database

---
+++ node-test/mysqlConnection.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+
+var dbConfig = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'bulletin_bord'
+    
+};
+
+var connection = mysql.createConnection(dbConfig);
+
+module.exports = connection;
