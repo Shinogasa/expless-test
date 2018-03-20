29a4ae41b6322b2332a4567ea1fc669f2107fc19
Shinonome <cuulbulu@gmail.com>
2018-03-20 05:17:58 +0000

    ログイン処理の実装

--- node-test/app.js
+++ node-test/app.js
@@ -4,11 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require("express-session");
 
-var index = require('./routes/index');
+var routes = require('./routes/index');
 var users = require('./routes/users');
 var boards = require('./routes/boards');
 var register = require('./routes/register');
+var login = require("./routes/login");
 
 var app = express();
 
@@ -23,11 +25,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
 
-app.use('/', index);
+app.use('/', routes);
 app.use('/users', users);
 app.use('/boards', boards);
 app.use('/register', register);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
