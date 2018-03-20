a79a1e0b498e1134ce2342d69e04b509ee26aa6c
Shinonome <cuulbulu@gmail.com>
2018-03-20 04:35:01 +0000

    routes修正

--- node-test/routes/register.js
+++ node-test/routes/register.js
@@ -13,11 +13,11 @@ router.post('/', function(req, res, next) {
     var userName = req.body.user_name;
     var email = req.body.email;
     var password = req.body.password;
-    var createAt = moment().format('YYYY-MM-DD HH:mm:ss');
+    var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
     var emailExistsQuery = 'SELECT * FROM users WHERE email = "' + email + '" LIMIT 1'; // 追加
     var registerQuery = 'INSERT INTO users (user_name, email, password, created_at) VALUES ("' + userName + '", ' + '"' + email + '", ' + '"' + password + '", ' + '"' + createdAt + '")'; // 変更
 
-    conection.query(emailExistsQuery, function(err, email) {
+    connection.query(emailExistsQuery, function(err, email) {
         var  emailExists = email.length == 1;
         if (emailExists) {
             res.render('register', {
