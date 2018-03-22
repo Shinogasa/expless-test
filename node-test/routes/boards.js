cf940d9e81d2d5151abbc6fe3ebdab8088c1fb37
Shinonome <cuulbulu@gmail.com>
2018-03-22 05:38:30 +0000

    board.jsをESLint

--- node-test/routes/boards.js
+++ node-test/routes/boards.js
@@ -1,41 +1,74 @@
-var express = require('express');
-var router = express.Router();
-var moment = require('moment');
-var multer = require("multer");
-var connection = require('../mysqlConnection');
-var upload = multer({ dest: './public/images/uploads/' });
-var cloudinary = require('cloudinary');
+const express = require('express');
+
+const router = express.Router();
+const moment = require('moment');
+
+const multer = require('multer');
+
+const connection = require('../mysqlConnection');
+
+const upload = multer({ dest: './public/images/uploads/' });
+const cloudinary = require('cloudinary');
+
 cloudinary.config({
   cloud_name: 'shinonome',
   api_key: '183642231461541',
   api_secret: 'Vk5UMzpW5XaOKXVV3h9mVdz_IVo'
 });
 
-router.get('/:board_id', function(req, res, next) {
-  var boardId = req.params.board_id;
-  var getBoardQuery = 'SELECT * FROM boards WHERE board_id = ' + boardId;
-  var getMessagesQuery = 'SELECT M.message, M.image_path, ifnull(U.user_name, \'名無し\') AS user_name, DATE_FORMAT(M.created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM messages M LEFT OUTER JOIN users U ON M.user_id = U.user_id WHERE M.board_id = ' + boardId + ' ORDER BY M.created_at ASC';    connection.query(getBoardQuery, function(err, board) {
-      connection.query(getMessagesQuery, function(err, messages) {
-        res.render('board', {
+router.get('/:board_id', (req, res) => {
+  const boardId = req.params.board_id;
+  const getBoardQuery = `
+    SELECT * 
+    FROM boards 
+    WHERE board_id = ${boardId}
+  `;
+  const getMessagesQuery = `
+    SELECT M.message, 
+           M.image_path, 
+           ifnull(U.user_name, '名無し') AS user_name, 
+           DATE_FORMAT(M.created_at, '%Y年%m月%d日 %k時%i分%s秒') AS created_at 
+    FROM messages M LEFT 
+                  OUTER JOIN users U 
+                          ON M.user_id = U.user_id 
+    WHERE M.board_id = ${boardId} 
+    ORDER BY M.created_at ASC
+  `;
+  connection.query(getBoardQuery, (getBoardQueryErr, board) => {
+    connection.query(getMessagesQuery, (getMessagesQueryErr, messages) => {
+      res.render('board', {
         title: board[0].title,
         board: board[0],
         messageList: messages
       });
-    }); 
+    });
   });
 });
 
-router.post('/:board_id', upload.single('image_file'), function(req, res) {
-  var path = req.file.path;
-  var message = req.body.message;
-  var boardId = req.params.board_id;
-  var userId = req.session.user_id? req.session.user_id: 0;
-  var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
-  cloudinary.uploader.upload(path, function(result) {
-    var imagePath = result.url;
-    var query = 'INSERT INTO messages (image_path, message, board_id, user_id, created_at) VALUES ("' + imagePath + '", ' + '"' + message + '", ' + '"' + boardId + '", ' + '"' + userId + '", ' + '"' + createdAt + '")';
-    connection.query(query, function(err, rows) {
-      res.redirect('/boards/' + boardId);
+
+router.post('/:board_id', upload.single('image_file'), (req, res) => {
+  const path = req.file.path;
+  const message = req.body.message;
+  const boardId = req.params.board_id;
+  const userId = req.session.user_id ? req.session.user_id : 0;
+  const createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+  cloudinary.uploader.upload(path, (result) => {
+    const imagePath = result.url;
+    const query = `
+        INSERT INTO messages
+                   (image_path,
+                    message,
+                    board_id,
+                    user_id,
+                    created_at)
+        VALUES    ('${imagePath}',
+                   '${message}',
+                   '${boardId}',
+                   '${userId}',
+                   '${createdAt}')
+    `;
+    connection.query(query, () => {
+      res.redirect(`/boards/${boardId}`);
     });
   });
 });
